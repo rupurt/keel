9d32b8990f39644a4dd17eb335231e3d29e8a07b
Alex Kwiatkowski <alex+git@fremantle.io>
2026-03-27 19:01:26 -0700

    Refine docs typography and spacing

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -31,14 +31,6 @@ const config: Config = {
     },
   },
 
-  stylesheets: [
-    {
-      href:
-        'https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500;9..144,600;9..144,700&family=IBM+Plex+Mono:wght@400;500;600&family=IBM+Plex+Sans:wght@400;500;600;700&display=swap',
-      type: 'text/css',
-    },
-  ],
-
   presets: [
     [
       'classic',
